3635db6e68b7bc59d8b3fbad90b7ac7e51a35b40
Mrrll <edgarmarrollo@gmail.com>
2021-07-29 04:24:11 +0200

    Doctrine en marcha ....

--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Dotenv\Dotenv;
+use App\Models\Product;
 require_once __DIR__ . '/../vendor/autoload.php';
 Dotenv::createImmutable(__DIR__ . '/../')->load();
-// Create a simple "default" Doctrine ORM configuration for Annotations
+
+// *: Cree una configuración ORM de Doctrine "predeterminada" simple para las anotaciones ...
 $paths = array(realpath(__DIR__ . '/../src/app/Models'));
 $isDevMode = true;
 $proxyDir = null;
@@ -19,7 +21,7 @@ $config = Setup::createAnnotationMetadataConfiguration(
     $useSimpleAnnotationReader
 );
 
-// database configuration parameters
+// ?: Parámetros de configuración de la base de datos ...
 $conn = [
     'dbname' => $_ENV['DATABASE_NAME'], // ?: DataBase Name
     'user' => $_ENV['DATABASE_USER'], // ?: DataBase Username
@@ -28,5 +30,15 @@ $conn = [
     'driver' => $_ENV['DATABASE_DRIVER'], // ?: DataBase Driver
 ];
 
-// obtaining the entity manager
+// ?: Obteniendo el administrador de la entidad ...
 $entityManager = EntityManager::create($conn, $config);
+
+// *: Creamos la Entidad Productos ...
+$newProductName = "Producto1";
+$product = new Product();
+$product->setName($newProductName);
+// *: Almacenamos los datos ...
+$entityManager->persist($product);
+$entityManager->flush();
+
+echo "Created Product with ID " . $product->getId() . "\n";
---
+++ src/app/Models/Product.php
@@ -0,0 +1,50 @@
+<?php
+// src/Product.php
+namespace App\Models;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    // .. (other code)
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
