2a2ed07e4bff11a3a45d8ee97e5e34aeca93a056
Mrrll <edgarmarrollo@gmail.com>
2021-07-29 20:33:28 +0200

    Listamos la Entidad Bug

---
+++ public/list_bugs.php
@@ -0,0 +1,21 @@
+<?php
+// TODO: Archivo de listar los bugs ...
+
+require_once __DIR__ . "/../config/bootstrap.php";
+
+// *: Listamos la Entidad Bug ...
+$dql = "SELECT b, e, r FROM App\Models\Bug b JOIN b.engineer e JOIN b.reporter r ORDER BY b.created DESC";
+
+$query = $entityManager->createQuery($dql);
+$query->setMaxResults(30);
+$bugs = $query->getResult();
+
+foreach ($bugs as $bug) {
+    echo $bug->getDescription()." - ".$bug->getCreated()->format('d.m.Y')."\n";
+    echo "    Reported by: ".$bug->getReporter()->getName()."\n";
+    echo "    Assigned to: ".$bug->getEngineer()->getName()."\n";
+    foreach ($bug->getProducts() as $product) {
+        echo "    Platform: ".$product->getName()."\n";
+    }
+    echo "\n";
+}
