ccc00d16acc8945523a4873144472273885990b2
Mrrll <edgarmarrollo@gmail.com>
2021-07-29 19:16:29 +0200

    Agregar errores y entidades de usuario

---
+++ src/app/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Models;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
