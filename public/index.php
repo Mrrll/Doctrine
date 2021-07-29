6492f0e4d3e0ffff14b13fc75555661bd010c2f2
Mrrll <edgarmarrollo@gmail.com>
2021-07-29 02:52:38 +0200

    Creando archivos de configuracion de doctrine

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+// TODO: Archivo unico de la app ...
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Dotenv\Dotenv;
+require_once __DIR__ . '/../vendor/autoload.php';
+Dotenv::createImmutable(__DIR__ . '/../')->load();
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+$config = Setup::createAnnotationMetadataConfiguration(
+    [realpath(__DIR__ . '/../src/Models')],
+    $isDevMode,
+    $proxyDir,
+    $cache,
+    $useSimpleAnnotationReader
+);
+
+// database configuration parameters
+$conn = [
+    'dbname' => $_ENV['DATABASE_NAME'], // ?: DataBase Name
+    'user' => $_ENV['DATABASE_USER'], // ?: DataBase Username
+    'password' => $_ENV['DATABASE_PASSWD'], // ?: DataBase Password
+    'host' => $_ENV['DATABASE_HOST'], // ?: DataBase Host
+    'driver' => $_ENV['DATABASE_DRIVER'], // ?: DataBase Driver
+];
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
